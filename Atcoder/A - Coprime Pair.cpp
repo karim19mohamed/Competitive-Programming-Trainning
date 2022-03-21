3c0d7b25c4011bf8d7dfca92db2c36c964f4aef6
Karim Mohamed <s.karim.ibrahim@gmail.com>
2022-03-21 03:54:44 +0200

    new solved

---
+++ Atcoder/A - Coprime Pair.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef complex<double> point;
+#define endl '\n'
+#define F first
+#define S second
+#define dbg(x) cerr<<#x<<" = "<<x<<endl
+#define dbg2(x,y) cerr<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<endl
+#define dbg3(x,y,z) cerr<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<", "<<#z<<" = "<<z<<endl
+#define dbg4(x,y,z,q) cerr<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<", "<<#z<<" = "<<z<<", "<<#q<<" = "<<q<<endl
+#define FOR(sz) for(int i = 0; i < sz; ++i)
+#define dbg_array(x,sz) FOR(sz) cerr << x[i] << " \n"[i==sz-1]
+#define X real()
+#define Y imag()
+
+const int d4i[4] = { -1, 0, 1, 0 }, d4j[4] = { 0, 1, 0, -1 };
+const int d8i[8] = { -1, -1, 0, 1, 1, 1, 0, -1 }, d8j[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+void _init_() {
+	ios_base::sync_with_stdio(0), cin.tie(0);
+#ifdef ECLIPSE
+	freopen("input.txt", "rt", stdin);
+//	freopen("output.txt", "wt", stdout);
+#endif
+}
+
+const int N = 2e4 + 9, M = 1e8 + 9, SEG = 131072; // SEG = 1 << ((int) ceil(log2(N)) + 1)
+const int Mod_M = 1e9 + 7;
+
+ll GCD(ll a, ll b) { // a>b
+	if (b == 0) return a;
+	return GCD(b, a % b);
+}
+
+void solve() {
+	ll l, r;
+	scanf("%lld%lld", &l, &r);
+	ll x = l, y = r, ans = r - l + 1;
+	bool flag = 0;
+	while (!flag) {
+		--ans;
+		for (ll i = l; i <= r; ++i) {
+			if (i + ans > r) {
+				break;
+			}
+			if (GCD(i + ans, i) == 1) {
+				flag = 1;
+				break;
+			}
+		}
+	}
+
+	printf("%lld\n", ans);
+//	cout << GCD(104, 21);
+}
+
+int main() {
+	_init_();
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+	solve();
+//	}
+	return 0;
+}
+
---
+++ Atcoder/C - Yamanote Line Game.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef complex<double> point;
+#define endl '\n'
+#define F first
+#define S second
+#define dbg(x) cerr<<#x<<" = "<<x<<endl
+#define dbg2(x,y) cerr<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<endl
+#define dbg3(x,y,z) cerr<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<", "<<#z<<" = "<<z<<endl
+#define dbg4(x,y,z,q) cerr<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<", "<<#z<<" = "<<z<<", "<<#q<<" = "<<q<<endl
+#define FOR(sz) for(int i = 0; i < sz; ++i)
+#define dbg_array(x,sz) FOR(sz) cerr << x[i] << " \n"[i==sz-1]
+#define X real()
+#define Y imag()
+
+const int d4i[4] = { -1, 0, 1, 0 }, d4j[4] = { 0, 1, 0, -1 };
+const int d8i[8] = { -1, -1, 0, 1, 1, 1, 0, -1 }, d8j[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+void _init_() {
+	ios_base::sync_with_stdio(0), cin.tie(0);
+#ifdef ECLIPSE
+	freopen("input.txt", "rt", stdin);
+//	freopen("output.txt", "wt", stdout);
+#endif
+}
+
+const int N = 1000 + 9, M = 1e8 + 9, SEG = 131072; // SEG = 1 << ((int) ceil(log2(N)) + 1)
+const int Mod_M = 1e9 + 7;
+
+bool played[N];
+
+void solve() {
+	int n;
+	scanf("%d", &n);
+	int other_player = -1;
+	while (other_player) {
+		for (int i = 1; i <= 2 * n + 1; ++i) {
+			if (!played[i]) {
+				played[i] = 1;
+				printf("%d\n", i);
+				fflush(stdout);
+				break;
+			}
+		}
+		scanf("%d", &other_player);
+		played[other_player] = 1;
+	}
+}
+
+int main() {
+	_init_();
+//	int t;
+//	scanf("%d", &t);
+//	while (t--) {
+	solve();
+//	}
+	return 0;
+}
+
